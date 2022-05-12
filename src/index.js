4338095b979bcbda536a83cf32b4a896785898fe
Bria222 <briantechologies.co.ke@gmail.com>
2022-05-12 11:48:26 +0300

    fetch data from an api using fetch method

--- src/index.js
+++ src/index.js
@@ -19,3 +19,16 @@ submitScore.addEventListener('click', async () => {
   inputNameField.value = '';
   inputScoreField.value = '';
 });
+
+refreshButton.addEventListener('click', () => {
+  scoreContainer.innerHTML = '';
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/llFawVoYD7zLAfvY9jQn/scores/')
+    .then((res) => res.json())
+    .then((data) => {
+      data.result.forEach((post) => {
+        const template = document.createElement('template');
+        template.innerHTML += `<p><span>${post.user}: </span> ${post.score}</p>`;
+        scoreContainer.appendChild(template.content);
+      });
+    });
+});
